7bbf076d39895039e94ac8f62cd0b7daae12d534
ChapCunningham <chapcunningham@gmail.com>
2025-07-25 15:45:55 -0400

    Update MAC_single_file.py

--- MAC_single_file.py
+++ MAC_single_file.py
@@ -356,4 +356,4 @@ if __name__ == "__main__":
     sys.stdout.flush()
     
     port = int(os.environ.get('PORT', 10000))
-    app.run_server(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=False)
